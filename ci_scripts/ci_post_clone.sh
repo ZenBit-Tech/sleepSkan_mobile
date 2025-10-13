10a933b5d0bc4ce7917e47888d3c08492695c817
Anastasiia Les <132888788+Anastasia-Les@users.noreply.github.com>
2025-10-13 11:46:47 +0300

    Add pod install to ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -e
 
-echo "📦 Installing CocoaPods dependencies..."
-
-# Fallback to current dir if not in Xcode Cloud
 REPO_PATH="${CI_PRIMARY_REPOSITORY_PATH:-$(pwd)}"
 cd "$REPO_PATH/ios"
 
+echo "📦 Installing CocoaPods..."
+
 gem install cocoapods --user-install || true
+
+# Optional: remove old Pods first
+rm -rf Pods
+
 pod install
 
-echo "✅ Pods installed successfully."
+echo "✅ Pods installed."
+
+echo "📁 Verifying generated Pod files..."
+ls -l Pods/Target\ Support\ Files/Pods-SleepScan/
